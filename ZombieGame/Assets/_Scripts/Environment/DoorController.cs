71e9756d513cff2f30c6b2a40362321a63bed094
ElPolGuai <polcomiporcar@gmail.com>
2023-10-13 17:02:56 +0200

    Door Controller :+1:

--- ZombieGame/Assets/_Scripts/Environment/DoorController.cs
+++ ZombieGame/Assets/_Scripts/Environment/DoorController.cs
@@ -11,7 +11,15 @@ public class DoorController : MonoBehaviour , IInteractable
     [SerializeField] private bool _opensOutwards = false;
     [SerializeField] private float _degToTurn = 160f;
 
- 
+    private int _speed = 100;
+    private bool _hasToMove = false;
+
+
+    private void Start()
+    {
+        if (_opensOutwards) _degToTurn *= -1;
+    }
+
     //interface method
     public void Interact()
     {
@@ -30,16 +38,31 @@ public class DoorController : MonoBehaviour , IInteractable
 
     void Open()
     {
-        this.transform.Rotate(new Vector3(0, 1, 0), _degToTurn);
+        _hasToMove = true;
     }
 
     void Close() {
-
+        _hasToMove = true;
+        _degToTurn = -_degToTurn;
     }
 
-    void changeBlockedStatus()
+    public void changeBlockedStatus()
     {
         _isBlocked = !_isBlocked;
+    }
+
+    private void Update()
+    {
+        if (_hasToMove)
+        {
+            if (this.transform.rotation.y < _degToTurn && this.transform.rotation.y > 0) {
+                //If the rotation of the door is not bigger than the total rotation it has to do to open or close itself.
+
+                this.transform.Rotate(Vector3.up, _degToTurn * Time.deltaTime * _speed);
+            }
+            else _hasToMove = false;
+            
+        }
     }
 }
 
