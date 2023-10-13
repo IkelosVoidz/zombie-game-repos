3a76d12a36f1eb0cef54a9f94ab9581fa3507dbc
IkelosVoidz <114417148+IkelosVoidz@users.noreply.github.com>
2023-10-13 19:36:05 +0200

    arreglado el isopen por defecto

--- ZombieGame/Assets/_Scripts/Environment/DoorController.cs
+++ ZombieGame/Assets/_Scripts/Environment/DoorController.cs
@@ -10,15 +10,15 @@ public class DoorController : MonoBehaviour , IInteractable
     [SerializeField] private bool _isOpen = false;
     [SerializeField] private bool _opensOutwards = false;
     [SerializeField] private float _degToTurn = 160f;
+    [SerializeField] private int _speed = 200;
 
-    float LERP_TIME = 0.01f;
-    int SPEED = 200;
-    float t = 0f;
-
+    private const float LERP_TIME = 0.01f;
+    private float t = 0f;
 
     private void Start()
     {
         if (_opensOutwards) _degToTurn *= -1;
+        if (_isOpen) transform.Rotate(new Vector3(0, 1, 0), _degToTurn); 
     }
 
     //interface method
@@ -34,7 +34,6 @@ public class DoorController : MonoBehaviour , IInteractable
                 Close();
             }
         }
-        
     }
 
     void Open()
@@ -53,10 +52,10 @@ public class DoorController : MonoBehaviour , IInteractable
 
     private void Update()
     {
-        if (_isOpen) transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(new Vector3(0, _degToTurn, 0)), LERP_TIME * Time.deltaTime * SPEED);
-        else transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(Vector3.zero), LERP_TIME * Time.deltaTime * SPEED);
+        if (_isOpen) transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(new Vector3(0, _degToTurn, 0)), LERP_TIME * Time.deltaTime * _speed);
+        else transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(Vector3.zero), LERP_TIME * Time.deltaTime * _speed);
 
-        t = Mathf.Lerp(t, LERP_TIME, Time.deltaTime * SPEED);
+        t = Mathf.Lerp(t, LERP_TIME, Time.deltaTime * _speed);
 
         if (t >= 1f)
         {
