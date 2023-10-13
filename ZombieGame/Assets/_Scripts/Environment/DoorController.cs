ef359337b0eb9c5a1f7623a478e8773b028cf025
ElPolGuai <polcomiporcar@gmail.com>
2023-10-13 15:50:27 +0200

    puerta (francisco :+1: )

--- ZombieGame/Assets/_Scripts/Environment/DoorController.cs
+++ ZombieGame/Assets/_Scripts/Environment/DoorController.cs
@@ -6,20 +6,40 @@ using UnityEngine.InputSystem.Processors;
 
 public class DoorController : MonoBehaviour , IInteractable
 {
-    [SerializeField] private bool _isBlocked;
-    [SerializeField] private bool _isOpen;
-    [SerializeField] private bool _opensOutwards;
-    [SerializeField] private float _degToTurn;
+    [SerializeField] private bool _isBlocked = false;
+    [SerializeField] private bool _isOpen = false;
+    [SerializeField] private bool _opensOutwards = false;
+    [SerializeField] private float _degToTurn = 160f;
 
-
+ 
     //interface method
     public void Interact()
     {
-       Open();
+        if (!_isBlocked) 
+        {
+            if (!_isOpen) {
+                Open();
+            }
+            else
+            {
+                Close();
+            }
+        }
+        
     }
 
     void Open()
     {
+        this.transform.Rotate(new Vector3(0, 1, 0), _degToTurn);
+    }
+
+    void Close() {
 
     }
+
+    void changeBlockedStatus()
+    {
+        _isBlocked = !_isBlocked;
+    }
 }
+
