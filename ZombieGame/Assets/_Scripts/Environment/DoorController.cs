7d7d22ee6ab2b479819c1fdbeb1ec4ae58114a7e
ElPolGuai <polcomiporcar@gmail.com>
2023-10-13 18:50:48 +0200

    Door Handling script cleaner

--- ZombieGame/Assets/_Scripts/Environment/DoorController.cs
+++ ZombieGame/Assets/_Scripts/Environment/DoorController.cs
@@ -12,10 +12,8 @@ public class DoorController : MonoBehaviour , IInteractable
     [SerializeField] private float _degToTurn = 160f;
 
     float LERP_TIME = 0.01f;
-    int SPEED = 100;
+    int SPEED = 200;
     float t = 0f;
-    
-    private bool _hasToMove = false;
 
 
     private void Start()
@@ -42,14 +40,10 @@ public class DoorController : MonoBehaviour , IInteractable
     void Open()
     {
         _isOpen = true;
-
-        _hasToMove = true;
     }
 
     void Close() {
         _isOpen = false;
-
-        _hasToMove = true;
     }
 
     public void changeBlockedStatus()
@@ -59,40 +53,15 @@ public class DoorController : MonoBehaviour , IInteractable
 
     private void Update()
     {
-        if (_hasToMove)
+        if (_isOpen) transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(new Vector3(0, _degToTurn, 0)), LERP_TIME * Time.deltaTime * SPEED);
+        else transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(Vector3.zero), LERP_TIME * Time.deltaTime * SPEED);
+
+        t = Mathf.Lerp(t, LERP_TIME, Time.deltaTime * SPEED);
+
+        if (t >= 1f)
         {
-            if (_isOpen) transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(new Vector3(0, _degToTurn, 0)), LERP_TIME * Time.deltaTime * SPEED);
-            else transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(Vector3.zero), LERP_TIME * Time.deltaTime * SPEED);
-
-            t = Mathf.Lerp(t, LERP_TIME, Time.deltaTime * SPEED);
-
-            if (t >= .7f)
-            {
-                t = 0f;
-            }
-
-            //this.transform.Rotate(Vector3.up, _degToTurn * Time.deltaTime * _speed);
-
-            //Debug.Log("y: " + this.transform.eulerAngles.y);
-           /* Debug.Log("deg to turn: " + _degToTurn);
-
-            if (_isOpen && this.transform.eulerAngles.y < _degToTurn)
-            {
-                this.transform.Rotate(Vector3.up, _degToTurn * Time.deltaTime * _speed);
-            }
-            else if (!_isOpen && this.transform.eulerAngles.y < _degToTurn)
-            else if
-                if (this.transform.eulerAngles.y < _degToTurn && this.transform.eulerAngles.y >= 0) {
-                //If the rotation of the door is not bigger than the total rotation it has to do to open or close itself.
-
-                if (_isOpen) this.transform.Rotate(Vector3.up, _degToTurn * Time.deltaTime * _speed);
-                
-                Debug.Log("ESTA ROTANDO");
-                Debug.Log("IS OPEN? " + _isOpen);
-            }
-            else _hasToMove = false;*/
-            
-        }
+            t = 0f;
+        }            
     }
 }
 
