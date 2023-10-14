4f7dcc79bc8498f42dc0988ffd39bd0f39720125
ElPolGuai <polcomiporcar@gmail.com>
2023-10-14 20:30:07 +0200

    Primeres portes interactuables al mapa

--- ZombieGame/Assets/_Scripts/Environment/DoorController.cs
+++ ZombieGame/Assets/_Scripts/Environment/DoorController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,18 +8,20 @@ using UnityEngine.InputSystem.Processors;
 public class DoorController : MonoBehaviour , IInteractable
 {
     [SerializeField] private bool _isBlocked = false;
-    [SerializeField] private bool _isOpen = false;
-    [SerializeField] private bool _opensOutwards = false;
-    [SerializeField] private float _degToTurn = 160f;
+    [SerializeField] private bool _opensOutwards = true;
+    [SerializeField] private float _degToTurn = 120f;
     [SerializeField] private int _speed = 200;
 
     private const float LERP_TIME = 0.01f;
     private float t = 0f;
+    private float _startingAngle;
+    private bool _isOpen = false;
+    
 
     private void Start()
     {
-        if (_opensOutwards) _degToTurn *= -1;
-        if (_isOpen) transform.Rotate(new Vector3(0, 1, 0), _degToTurn); 
+        _startingAngle = transform.localEulerAngles.y;
+        if (_opensOutwards) _degToTurn *= -1; 
     }
 
     //interface method
@@ -52,15 +55,23 @@ public class DoorController : MonoBehaviour , IInteractable
 
     private void Update()
     {
-        if (_isOpen) transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(new Vector3(0, _degToTurn, 0)), LERP_TIME * Time.deltaTime * _speed);
-        else transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(Vector3.zero), LERP_TIME * Time.deltaTime * _speed);
+        if (_isOpen)
+        {
+            Vector3 targetRotation = new Vector3(0, _degToTurn + _startingAngle, 0);
+            transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, targetRotation, LERP_TIME * Time.deltaTime * _speed);
+        }
+        else
+        {
+            Vector3 targetRotation = new Vector3(0, _startingAngle, 0);
+            transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, targetRotation, LERP_TIME * Time.deltaTime * _speed);
+        }
 
         t = Mathf.Lerp(t, LERP_TIME, Time.deltaTime * _speed);
 
         if (t >= 1f)
         {
             t = 0f;
-        }            
+        }
     }
 }
 
