3c71b3f5380277c23abb9ccd9a44460e76b7b7a5
FerranPerezGarcia <u1980756@campus.udg.edu>
2023-10-26 22:16:21 +0200

    ZOMBIEEEE :dog:

--- ZombieGame/Assets/_Scripts/Units/Zombie/Zombie.cs
+++ ZombieGame/Assets/_Scripts/Units/Zombie/Zombie.cs
@@ -25,7 +25,7 @@ public class Zombie : MonoBehaviour
     [SerializeField] GameObject target;
     [SerializeField] Animator animator;
     [SerializeField] HealthComponent healthComponent;
-    private string[] attackAnimations = new string[] {"sergi","pau" };
+    private string[] attackAnimations = new string[] {"","" };
 
     private float maxSpeed;
 
