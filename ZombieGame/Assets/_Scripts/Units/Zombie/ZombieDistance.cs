52fbb288acc239bade1814407194fa894d07a5f0
ivanromontil <u1979279@campus.udg.edu>
2023-11-30 17:30:25 +0100

    FIUMBA zombie a distancia v2

--- ZombieGame/Assets/_Scripts/Units/Zombie/ZombieDistance.cs
+++ ZombieGame/Assets/_Scripts/Units/Zombie/ZombieDistance.cs
@@ -4,9 +4,45 @@ using UnityEngine;
 
 public class ZombieDistance : Zombie
 {
-    public void testingThingsMaNiggaMAN()
+    private float timeBetweenAttacks;
+    private bool alreadyAttacked;
+
+    private float attackRange = 5f;
+    private bool playerInAttackRange;
+    [SerializeField] GameObject projectile;
+
+    [SerializeField] LayerMask whatIsGround, whatIsPlayer;
+
+    protected override void Update()
     {
-        Debug.Log("No se que hago aquí pero me llamo francisco");
+
+        base.Update();
+
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+        if (playerInAttackRange)
+        {
+            if(state!=BUSY)Attack();
+            Debug.Log("ESTA DENTRO XAVALE");
+        }
+        else
+            Debug.Log("ESTA FUERA XAVALE");
+
+
+    }
+
+    public override void Attack()
+    {
+        navMeshAgent.speed = 0;
+        animator.Play("ATTACK" + Random.Range(1, 6));
+        state = BUSY;
+
+        Vector3 throwPos = new Vector3(transform.position.x, transform.position.y+1,transform.position.z);
+        transform.LookAt(target.transform);
+
+        Rigidbody rb = Instantiate(projectile,throwPos,Quaternion.identity).GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * 15f, ForceMode.Impulse);
+        rb.AddForce(transform.up * 5f, ForceMode.Impulse);
+        
     }
 
 }
