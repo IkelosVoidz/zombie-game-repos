d32e441da835d9c377c91815ba40f38bd3636ca9
ElPolGuai <polcomiporcar@gmail.com>
2023-12-09 15:13:43 +0100

    Todas las misiones hechas nashei

--- ZombieGame/Assets/_Scripts/Environment/PotionHandler.cs
+++ ZombieGame/Assets/_Scripts/Environment/PotionHandler.cs
@@ -4,9 +4,16 @@ using UnityEngine.Events;
 public class PotionHandler : MonoBehaviour, IInteractable
 {
     public UnityEvent onPotionMakerUsed;
+    public UnityEvent onPotionMakerEnded;
 
     public void Interact()
     {
         onPotionMakerUsed?.Invoke();
     }
+
+    public void OnTimerEnded()
+    {
+        onPotionMakerEnded?.Invoke();
+    }
+
 }
