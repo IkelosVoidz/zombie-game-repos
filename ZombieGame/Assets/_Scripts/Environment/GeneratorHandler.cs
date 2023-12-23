c615dbc3a3f99536d4e0ffb126b88e4b923d8834
IkelosVoidz <114417148+IkelosVoidz@users.noreply.github.com>
2023-12-23 21:21:31 +0100

    algunos bugs arreglaos

--- ZombieGame/Assets/_Scripts/Environment/GeneratorHandler.cs
+++ ZombieGame/Assets/_Scripts/Environment/GeneratorHandler.cs
@@ -1,15 +1,17 @@
-using JetBrains.Annotations;
-using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class GeneratorHandler : MonoBehaviour, IInteractable
 {
     public UnityEvent onGeneratorUsed;
+    public bool oneTime;
+    bool active;
 
     public void Interact()
     {
-        onGeneratorUsed?.Invoke();
-    }
+        if (oneTime && active) return;
 
+        onGeneratorUsed?.Invoke();
+        active = !active;
+    }
 }
